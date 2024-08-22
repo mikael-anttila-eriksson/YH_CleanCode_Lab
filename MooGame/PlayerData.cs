24eaccf2b69c035818786b8e0d3eec795270ba24
Alexander-itHs <143017808+Alexander-itHs@users.noreply.github.com>
2024-08-22 16:09:56 +0200

    Minor changes Made TotalGuesses into a property. Changed some variable names.

--- MooGame/PlayerData.cs
+++ MooGame/PlayerData.cs
@@ -6,43 +6,43 @@ using System.Threading.Tasks;
 
 namespace MooGame
 {
-	internal class PlayerData
-	{
-		
+    public class PlayerData
+    {
+
         public string Name { get; private set; }
-		public int NGames { get; private set; }
-		int totalGuess;
+        public int NumGames { get; private set; }
+        public int TotalGuesses { get; private set; }
 
 
-		public PlayerData(string name, int guesses)
-		{
-			this.Name = name;
-			NGames = 1;
-			totalGuess = guesses;
-		}
+        public PlayerData(string name, int guesses)
+        {
+            Name = name;
+            NumGames = 1;
+            TotalGuesses = guesses;
+        }
 
-		public void Update(int guesses)
-		{
-			totalGuess += guesses;
-			NGames++;
-		}
+        public void Update(int guesses)
+        {
+            TotalGuesses += guesses;
+            NumGames++;
+        }
 
-		public double Average()
-		{
-			return (double)totalGuess / NGames;
-		}
-
-
-		public override bool Equals(Object p)
-		{
-			return Name.Equals(((PlayerData)p).Name);
-		}
+        public double Average()
+        {
+            return (double)TotalGuesses / NumGames;
+        }
 
 
-		public override int GetHashCode()
-		{
-			return Name.GetHashCode();
-		}
-	}
+        public override bool Equals(object p)
+        {
+            return Name.Equals(((PlayerData)p).Name);
+        }
+
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
 
 }
