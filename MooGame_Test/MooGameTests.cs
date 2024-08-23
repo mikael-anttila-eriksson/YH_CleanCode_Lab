e46e6245fc10dc1b25b3833e1765fc74e866fe87
Alexander-itHs <143017808+Alexander-itHs@users.noreply.github.com>
2024-08-23 14:52:55 +0200

    Added test for MooGame class. EvaluateBullsCowsTest

---
+++ MooGame_Test/MooGameTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MooGame.Business;
+
+namespace MooGame_Test
+{
+    [TestClass()]
+    public class MooGameTests
+    {
+        [TestMethod()]
+        [DataRow("1234", "2835", "BC")]
+		[DataRow("1234", "1234", "BBBB")]
+        [DataRow("1234", "4321", "CCCC")]
+        [DataRow("1234", "5678", "")]
+		public void EvaluateBullsCowsTest(string correctAnswer, string guess, string expectedOutput)
+        {
+            // Arrange
+            MooGame.Business.MooGame mooGame = new MooGame.Business.MooGame();
+			
+            //string correctAnswer = "1234";
+			//string guess = "2835";
+
+			// Act
+			string actualOutput = mooGame.EvaluateBullsCows(correctAnswer, guess);
+            //string expectedOutput = "BC";
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput, "Incorrect evaluation.");
+        }
+    }
+}
