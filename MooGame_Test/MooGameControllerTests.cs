c9ea28ffda60db4cd538fb3fb35c70450d9c1361
Alexander-itHs <143017808+Alexander-itHs@users.noreply.github.com>
2024-08-23 13:16:11 +0200

    Added test project.

---
+++ MooGame_Test/MooGameControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MooGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MooGame.Tests
+{
+	[TestClass()]
+	public class MooGameControllerTests
+	{
+		[TestMethod()]
+		public void MooGameControllerTest()
+		{
+			// Arrange
+			var ui = new UI();
+			var gameController = new MooGameController(ui);
+
+			// Act
+			gameController.Run();
+
+			// Assert
+			Assert.Fail();
+		}
+	}
+}
---
+++ MooGame_Test/PlayerDataTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MooGame.Tests
+{
+    [TestClass()]
+	public class PlayerDataTests
+	{
+		[TestMethod()]
+		public void UpdateTest()
+		{
+			// Arrange: Initialize the object to be tested
+			int guesses = 5;
+			PlayerData playerData = new PlayerData("Erik", guesses);
+
+			// Act: Act on the object --> call a method
+			int newGuesses = 3;
+			playerData.Update(newGuesses);
+			int actualGuesses = playerData.TotalGuesses;
+			int expectedGuesses = guesses + newGuesses;
+
+			// Assert: Verify
+			Assert.AreEqual(expectedGuesses, actualGuesses, "Total guesses not correct.");
+		}
+		[TestMethod()]
+		public void AverageTest()
+		{
+			// Arrange: Initialize the object to be tested
+			int guesses = 5;
+			PlayerData playerData = new PlayerData("Erik", guesses);
+
+			// Act: Act on the object --> call a method
+			int newGuesses = 3;
+			playerData.Update(newGuesses);
+			double expectedAverage = 4;
+			double actualAverage = playerData.Average();
+
+			// Assert: Verify
+			Assert.AreEqual(expectedAverage, actualAverage, "Incorrect average.");
+		}
+
+	}
+}
