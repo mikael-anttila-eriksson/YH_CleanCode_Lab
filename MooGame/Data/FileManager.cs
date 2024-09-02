30366b5b94ac1f4a4e90eb3550c7f8e4e2e022d4
MikeBikeX <anttila_micke@hotmail.com>
2024-09-02 10:15:37 +0200

    Refactored FileManager - ReadData()

--- MooGame/Data/FileManager.cs
+++ MooGame/Data/FileManager.cs
@@ -6,44 +6,47 @@ using System.Threading.Tasks;
 
 namespace MooGame.Data
 {
-	public class FileManager : IFileManger
+	public class FileManager : IFileManager
 	{
         public FileManager()
         {
             
         }
-        public void SavePlayerScore(string name, int numGuesses)
+        public void SavePlayerScore(string name, int numGuesses, string path)
 		{
-			StreamWriter output = new StreamWriter("result.txt", append: true);
+			StreamWriter output = new StreamWriter(path, append: true);
 			output.WriteLine(name + "," + numGuesses);
 			output.Close();
 		}
-		public List<PlayerData> ReadData()
+		public List<string[]> ReadData(string path)
 		{
 			List<PlayerData> playerStatistics = new List<PlayerData>();
-			using (StreamReader reader = new StreamReader("result.txt"))
+			List<string[]> playerString = new List<string[]>();
+			using (StreamReader reader = new StreamReader(path))
 			{				
 				string line;
 				while ((line = reader.ReadLine()) != null)
 				{
-					string[] nameAndScore = line.Split(new string[] { "," }, StringSplitOptions.None);
-					string name = nameAndScore[0];
-					int guesses = Convert.ToInt32(nameAndScore[1]);
-					PlayerData playerData = new PlayerData(name, guesses);
-					int positionInList = playerStatistics.IndexOf(playerData);
-					if (positionInList < 0)
-					{
-						playerStatistics.Add(playerData);
-					}
-					else
-					{
-						playerStatistics[positionInList].Update(guesses);
-					}
+					string[] nameAndScore = line.Split(new string[] { "," }, StringSplitOptions.TrimEntries);
+					playerString.Add(nameAndScore);
+					//$"{nameAndScore[0]}, {nameAndScore[1]}"
+					//string name = nameAndScore[0];
+					//int guesses = Convert.ToInt32(nameAndScore[1]);
+					//PlayerData playerData = new PlayerData(name, guesses);
+					//int positionInList = playerStatistics.IndexOf(playerData);
+					//if (positionInList < 0)
+					//{
+					//	playerStatistics.Add(playerData);
+					//}
+					//else
+					//{
+					//	playerStatistics[positionInList].Update(guesses);
+					//}
 				}				
 			};
 				
 			
-			return playerStatistics;
+			return playerString;
 		}
 	}
 }
